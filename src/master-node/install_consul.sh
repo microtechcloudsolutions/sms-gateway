ae484b69d194076c6d8cc3d02d7f2d5b09994b5a
leemwlando <leemwlando@gmail.com>
2019-04-09 17:27:08 +0200

    fix-2-scripts-scirpts-ports

--- src/master-node/install_consul.sh
+++ src/master-node/install_consul.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # "skip_leave_on_interrupt": true
-docker run -d -p $1:$2 --name=consul --net=host consul agent -server -advertise 127.0.0.1
+docker run -d -p 8500:8500 --name=consul --net=host consul agent -server -advertise 127.0.0.1
