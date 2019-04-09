3ecb5d132002c3163dc877c9b41000e382d99167
leemwlando <leemwlando@gmail.com>
2019-04-09 17:22:46 +0200

    fix-2-scripts-consul-advertise

--- src/master-node/install_consul.sh
+++ src/master-node/install_consul.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # "skip_leave_on_interrupt": true
-docker run -d -p $1:$2 --name=consul --net=host consul agent -server -advertise
+docker run -d -p $1:$2 --name=consul --net=host consul agent -server -advertise 127.0.0.1
