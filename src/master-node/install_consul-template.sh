cbc5e44b5d39cd3fbf7b921b8f6d3075ea6da87e
leemlwando <leemlwando@gmail.com>
2019-04-11 15:51:31 +0200

    fix directories

--- src/master-node/install_consul-template.sh
+++ src/master-node/install_consul-template.sh
@@ -5,7 +5,7 @@ curl -O https://releases.hashicorp.com/consul-template/0.20.0/consul-template_0.
 echo "***===============Unpacking Consul Template===============***"
 tar -zxf consul-template_0.20.0_linux_amd64.tgz -C /usr/local/bin
 echo "***===============Running Consul Template===============***"
-consul-template -config="./consul-template.conf"
+consul-template -config="/sourcecode/master-node/consul-template.conf" -once
 echo "***===============Cleaning *tgz files===============***"
 rm consul-template_0.20.0_linux_amd64.tgz
 echo "************************************************************"
